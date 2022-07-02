71eeddd05fdf1369d6a587aea0514a3966f6b225
Suraj Mothilal <surajmothilal@surajsmacstudio.home>
2022-07-02 14:12:47 -0400

    Adds timestamps to database tables and their triggers

--- backend/data/migrations/20220418161735_createUser.js
+++ backend/data/migrations/20220418161735_createUser.js
@@ -1,7 +1,19 @@
-exports.up = knex =>
-  knex.schema.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"').createTable('users', tbl => {
+const tableName = 'users';
+
+exports.up = async knex => {
+  await knex.schema.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"').createTable(tableName, tbl => {
     tbl.uuid('id').primary().defaultTo(knex.raw('uuid_generate_v4()'))
     tbl.text('name', 128).notNullable()
-})
+    tbl.timestamps(false, true)
+  })
 
-exports.down = knex => knex.schema.dropTableIfExists('users')
+  await knex.raw(`
+    CREATE TRIGGER update_timestamp
+    BEFORE UPDATE
+    ON ${tableName}
+    FOR EACH ROW
+    EXECUTE PROCEDURE update_timestamp();
+  `);
+}
+
+exports.down = knex => knex.schema.dropTableIfExists(tableName)
--- backend/data/migrations/20220418163334_createAccount.js
+++ backend/data/migrations/20220418163334_createAccount.js
@@ -1,11 +1,23 @@
-exports.up = knex =>
-  knex.schema.createTable('accounts', tbl => {
+const tableName = 'accounts'
+
+exports.up = async knex => {
+  await knex.schema.createTable(tableName, tbl => {
     tbl.uuid('id').primary().defaultTo(knex.raw('uuid_generate_v4()'))
     tbl.text('name', 128).notNullable()
     tbl.decimal('balance', 15, 2).notNullable()
     tbl.text('currency', 3).notNullable()
     tbl.text('type', 50).notNullable()
     tbl.uuid('userId').notNullable().references('id').inTable('users').onDelete('CASCADE')
-})
+    tbl.timestamps(false, true)
+  })
 
-exports.down = knex => knex.schema.dropTableIfExists('accounts')
+  await knex.raw(`
+    CREATE TRIGGER update_timestamp
+    BEFORE UPDATE
+    ON ${tableName}
+    FOR EACH ROW
+    EXECUTE PROCEDURE update_timestamp();
+  `);
+}
+
+exports.down = knex => knex.schema.dropTableIfExists(tableName)
